3aae0dea1cc21f4c8148f2be03a4c94ceaebdfb4
Petr Reznikov <prez@yandex-team.ru>
2016-06-03 17:03:50 +0300

    Updated share folder, remove cmake_helpers
    Change-Id: I589d179d09a425230e9d6e13f0e761ff52c1ed7d

--- share/find_recipient_parser_dependencies.cmake
+++ share/find_recipient_parser_dependencies.cmake
@@ -1,9 +1,15 @@
 function(find_recipient_parser_dependencies)
 
-set(DEPS
-  MYBOOST
+find_path(BOOST_INCLUDE_DIR
+  NAMES boost/spirit/include/qi.hpp
+  HINTS ${CMAKE_INSTALL_PREFIX}/include/system
 )
 
-yamail_find_dependencies(RECIPIENT_PARSER "${DEPS}" "")
+set(RECIPIENT_PARSER_DEPENDENCY_INCLUDE_DIRS
+    ${BOOST_INCLUDE_DIR}
+    CACHE INTERNAL ""
+)
+
+message(STATUS "Found depenedency include dirs: ${RECIPIENT_PARSER_DEPENDENCY_INCLUDE_DIRS}")
 
 endfunction()
